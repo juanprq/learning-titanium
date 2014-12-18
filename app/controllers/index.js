67bdab7db8f7b016510619000534e55828ec69ad
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2014-12-18 15:53:17 -0500

    Added feature to add books to the collection

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,3 +6,22 @@ var book = Alloy.createModel('books', {
   title: 'Great expectations',
   author: 'Charles Dickens'
 });
+
+myBooks.add(book);
+book.save();
+
+function showBook(event) {
+ var selectedBook = event.source;
+ var args = {
+   title: selectedBook.title,
+   author: selectedBook.author
+ };
+ 
+ var bookView = Alloy.createController("bookdetails", args).getView();
+ bookView.open();
+}
+
+function addBook () {
+  var myAddBook = Alloy.createController("addbook", {}).getView();
+  myAddBook.open();
+}
